f951302cc55ad5ee06d78e756f1c73c5ad8078f8
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-02 19:47:53 +0300

    Update image.py

--- image.py
+++ image.py
@@ -11,7 +11,7 @@ class Widget(Gtk.VBox):
         Gtk.VBox.__init__(self, spacing=1, halign=Gtk.Align.CENTER)
         pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
         filename=picture, 
-        width=100, 
+        width=120, 
         height=100, 
         preserve_aspect_ratio=True)
         image = Gtk.Image.new_from_pixbuf(pixbuf)
@@ -19,34 +19,34 @@ class Widget(Gtk.VBox):
         self.add(image)
         print(picture)
         
-       
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title='GMouse 600')
         self.set_border_width(10)
-        self.set_default_size(800, 700)
+        self.set_default_size(930, 600)
         self.deneme = []
         self.backgrounds = "/usr/share/backgrounds"
+        self.wallpaper_select_index = 0
         self.data = load()
         
-
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
         hb.props.title = "Wallpaper"
         self.set_titlebar(hb)
-        
-        currencies = ["Centered", "Zoom", "Spanned"]
+ 
         self.currency_combo = Gtk.ComboBoxText()
         self.currency_combo.connect("changed", self.on_currency_combo_changed)
-        for currency in currencies:
-            self.currency_combo.append_text(currency)
+        for select in ["None", "Centered", "Zoom", "Spanned", "Wallpaper", "Scaled", "Stretched"]:
+            self.currency_combo.append_text(select)
         self.currency_combo.set_entry_text_column(0)
         hb.pack_end(self.currency_combo)
-        self.currency_combo.set_active(int(self.data.get("index")))
+        if self.data.get("index") == "None":  self.currency_combo.set_active(int(index))
+        else:
+            self.currency_combo.set_active(int(self.data.get("index")))
              
-        button=Gtk.Button(label="")
+        button=Gtk.Button(label="Cancel")
         button.connect('clicked', self.info)
-        hb.pack_end(button)
+        hb.pack_start(button)
 
         scrollbox = Gtk.ScrolledWindow(visible=False)
         self.add(scrollbox)
@@ -62,21 +62,27 @@ class MainWindow(Gtk.Window):
             
     def test(self, deneme, heyo):        
         wallpaper = self.deneme[heyo.get_index()]
+        self.wallpaper_select_index = heyo.get_index()
         settings = Gio.Settings.new("org.gnome.desktop.background")
         settings.set_string("picture-uri", wallpaper)
         settings.apply()
+  
         
     def on_currency_combo_changed(self, combo):
         text = combo.get_active_text()
-        index = combo.get_active()
         if text is not None:
-            print(text.lower())
             self.data.set("duzen", text.lower())
-            self.data.set("index", index)
+            self.data.set("index", int(combo.get_active()))
+        print ("walpaper select index", text)
 
+        settings = Gio.Settings.new("org.gnome.desktop.background")
+        settings.set_string("picture-options", text.lower())
+        settings.apply()
+            
 
+        
     def info(self, widged, data=None):
-        print (self.data.get("duzen"))
+        return 0
         
 
 window = MainWindow()
