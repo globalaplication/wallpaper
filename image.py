7ea79ae1229c02826db2a6a4190de18b1632ff70
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-02 18:44:52 +0300

    Update image.py

--- image.py
+++ image.py
@@ -4,9 +4,11 @@ gi.require_version('Gtk', '3.0')
 import os
 from gi.repository import Gtk, GdkPixbuf, Gio
 
+from config import load
+
 class Widget(Gtk.VBox):
     def __init__(self, picture):
-        Gtk.VBox.__init__(self, spacing=1, halign=Gtk.Align.START)
+        Gtk.VBox.__init__(self, spacing=1, halign=Gtk.Align.CENTER)
         pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
         filename=picture, 
         width=100, 
@@ -17,23 +19,32 @@ class Widget(Gtk.VBox):
         self.add(image)
         print(picture)
         
-        g = Gtk.Label("wallpaper")
-        self.add(g)
-        
+       
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title='GMouse 600')
         self.set_border_width(10)
-        self.set_default_size(800, 550)
+        self.set_default_size(800, 700)
         self.deneme = []
         self.backgrounds = "/usr/share/backgrounds"
+        self.data = load()
+        
 
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
-        hb.props.title = "HeaderBar example"
+        hb.props.title = "Wallpaper"
         self.set_titlebar(hb)
+        
+        currencies = ["Centered", "Zoom", "Spanned"]
+        self.currency_combo = Gtk.ComboBoxText()
+        self.currency_combo.connect("changed", self.on_currency_combo_changed)
+        for currency in currencies:
+            self.currency_combo.append_text(currency)
+        self.currency_combo.set_entry_text_column(0)
+        hb.pack_end(self.currency_combo)
+        self.currency_combo.set_active(int(self.data.get("index")))
              
-        button=Gtk.Button(label="info")
+        button=Gtk.Button(label="")
         button.connect('clicked', self.info)
         hb.pack_end(button)
 
@@ -54,10 +65,18 @@ class MainWindow(Gtk.Window):
         settings = Gio.Settings.new("org.gnome.desktop.background")
         settings.set_string("picture-uri", wallpaper)
         settings.apply()
+        
+    def on_currency_combo_changed(self, combo):
+        text = combo.get_active_text()
+        index = combo.get_active()
+        if text is not None:
+            print(text.lower())
+            self.data.set("duzen", text.lower())
+            self.data.set("index", index)
 
 
     def info(self, widged, data=None):
-        return 0
+        print (self.data.get("duzen"))
         
 
 window = MainWindow()
@@ -65,9 +84,5 @@ window.connect('delete-event', Gtk.main_quit)
 window.show_all()
 Gtk.main()
 
-
-
-
-#https://stackoverflow.com/questions/28551977/python-gtk3-limit-label-width
 
 
