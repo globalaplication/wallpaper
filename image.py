3179ef9de1f292a3820b305a0e939c3050152585
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-02 15:44:26 +0300

    Update image.py

--- image.py
+++ image.py
@@ -2,32 +2,25 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf
-
 import os
-
-deneme = []
-
 class Widget(Gtk.VBox):
     def __init__(self, picture):
         Gtk.VBox.__init__(self)
-
         pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
         filename=picture, 
         width=100, 
         height=100, 
         preserve_aspect_ratio=True)
-
         image=Gtk.Image.new_from_pixbuf(pixbuf)
         image.set_size_request(10, 10)
         #image.set_from_file(image)
         self.add(image)
-        
         print(picture)
-
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title='GMouse 600')
         self.set_default_size(800, 550)
+        self.deneme = []
 
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
@@ -48,15 +41,17 @@ class MainWindow(Gtk.Window):
         scrollbox.add(self.flowbox)
         for i in os.listdir("/usr/share/backgrounds"):
             w=Widget("/usr/share/backgrounds/"+i)
-            deneme.append("/usr/share/backgrounds/"+i)
+            self.deneme.append("/usr/share/backgrounds/"+i)
             self.flowbox.add(w)
             
     def test(self, deneme, heyo):
-        print (heyo.get_index())
-        
+
         gnome = "gsettings set org.gnome.desktop.background picture-uri"
-        wallpaper = deneme[1]
+        
+        wallpaper = self.deneme[heyo.get_index()]
+        
         string = "{} {}".format(gnome, wallpaper)
+        
         os.system(string)
         
 
